7de6773cc5e664820978966ae8f092805daf9021
HanniJaviera <javiera.arc@hotmail.com>
2025-09-03 22:00:37 -0400

    Co-authored-by: GenAlarcon <GenAlarcon@users.noreply.github.com> Co-authored-by: Samuel Bueno <Creative-Duoc@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tienda_Guau_-_Miau
 https://prod.liveshare.vsengsaas.visualstudio.com/join?922BB8CD0B343AD8387ABBF42851000D1B1F
+
+https://lvc4mcrq-3000.brs.devtunnels.ms/
---
+++ script.js
@@ -0,0 +1,8 @@
+
+function hacerClickRegistrar(){
+    var formulario = document.getElementById('formularioRegistro');
+    formulario.style.display = 'block'; // Lo hace visible
+}
+function hacerClickIniciarSesion(){
+    alert('¡Se ha hecho clic en el botón!');
+}
